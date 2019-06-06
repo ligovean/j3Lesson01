592a9be31b4b36336cb00cda423c260750ea0460
ligovean <ligomail@rambler.ru>
2019-06-06 22:23:27 +0300

    Task03 Complite

--- src/main/java/task03/Main.java
+++ src/main/java/task03/Main.java
@@ -3,6 +3,8 @@ package task03;
 import task03.Content.Apple;
 import task03.Content.Orange;
 
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -25,10 +27,21 @@ public class Main {
         }
         boxOfApple1.putFruit(new Apple(5.1f));  //Положим в коробку одно огромное яблоко чтобы уровнять вес коробок
 
-        System.out.printf("Вес коробки с апельсинами: %.2f \n", boxOfOrange1.getWeight());
-        System.out.printf("Вес коробки с яблоками: %.2f \n", boxOfApple1.getWeight());
+        System.out.printf("Вес коробки boxOfOrange1 с апельсинами: %.2f \n", boxOfOrange1.getWeight());
+        System.out.printf("Вес коробки boxOfApple1 с яблоками: %.2f \n", boxOfApple1.getWeight());
 
         System.out.println("Вес коробок равен? " + boxOfApple1.compare(boxOfOrange1));
+
+
+        //Пустая коробка для яблок
+        Box<Apple> boxOfApple2 = new Box<Apple>();
+
+        System.out.printf("Вес коробки boxOfApple2 с яблоками: %.2f \n", boxOfApple2.getWeight());
+        System.out.println("Пересыпаем из boxOfApple1 в boxOfApple2");
+        boxOfApple1.pour(boxOfApple2); //Пересыпим всё из коробки boxOfApple1 в коробку boxOfApple2
+
+        System.out.printf("Вес коробки boxOfApple1 после пересыпания: %.2f \n", boxOfApple1.getWeight());
+        System.out.printf("Вес коробки boxOfApple2 после пересыпания: %.2f \n", boxOfApple2.getWeight());
 
     }
 }
